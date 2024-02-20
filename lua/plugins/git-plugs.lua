c35a388854969561c229aceaf2a87f751ff45791
mmourazos <mourazos@edu.xunta.gal>
2024-02-20 04:10:02 +0100

    voy para camita.

--- lua/plugins/git-plugs.lua
+++ lua/plugins/git-plugs.lua
@@ -5,8 +5,15 @@ return {
       require("gitsigns").setup()
     end,
     keys = {
-      { "n", "<leader>gj", "<cmd>lua require('gitsigns').next_hunk()<cr>", desc = "gitsigns next hunk" },
-      { "n", "<leader>gk", "<cmd>lua require('gitsigns').prev_hunk()<cr>", desc = "gitsigns prev. hunk" },
+      { "<leader>gj", "<cmd>Gitsigns next_hunk<cr>", desc = "gitsigns next hunk" },
+      { "<leader>gk", "<cmd>Gitsigns prev_hunk<cr>", desc = "gitsigns prev. hunk" },
+      { "<leader>gp", "<cmd>Gitsigns preview_hunk<cr>", desc = "gitsigns preview hunk" },
+      { "<leader>gr", "<cmd>Gitsigns reset_hunk<cr>", desc = "gitsigns reset hunk" },
+      { "<leader>gR", "<cmd>Gitsigns reset_buffer<cr>", desc = "gitsigns reset buffer" },
+      { "<leader>gs", "<cmd>Gitsigns stage_hunk<cr>", desc = "gitsigns stage hunk" },
+      { "<leader>gu", "<cmd>Gitsigns undo_stage_hunk<cr>", desc = "gitsigns undo stage hunk" },
+      { "<leader>gU", "<cmd>Gitsigns reset_buffer_index<cr>", desc = "gitsigns reset buffer index" },
+      { "<leader>g?", "<cmd>Gitsigns toggle_signs<cr>", desc = "gitsigns toggle signs" },
     },
   },
   {
